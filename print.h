9911436e8cf333ed46b52c609977caa709ed99e6
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-07-06 19:25:15 +0200

    Removed usage of __VA_OPT__

--- print.h
+++ print.h
@@ -7,19 +7,16 @@
 #include <stdio.h>
 
 #define WRITE(Stream, ...) do { \
-	_Pragma("GCC diagnostic push"); \
-	_Pragma("GCC diagnostic ignored \"-Wformat\""); \
 	FILE *const PRINT_WRITE_STREAM_ = (Stream); \
 	PRINT_IF_(PRINT_IS_PACK_     (PRINT_LAST_(__VA_ARGS__))) ( \
 		PRINT_EVAL_(PRINT_WRITE0_(PRINT_LAST_(__VA_ARGS__), __VA_ARGS__)) \
 	) ( \
 		PRINT_EVAL_(PRINT_WRITE3_(__VA_ARGS__)) \
 	) \
-	_Pragma("GCC diagnostic pop"); \
 } while (0)
 #define WRITELN(Stream, ...) do { \
 	FILE *const PRINT_WRITELN_STREAM_ = (Stream); \
-	__VA_OPT__(WRITE(PRINT_WRITELN_STREAM_, __VA_ARGS__);) \
+	PRINT_IF_(PRINT_NO_ARGS_(__VA_ARGS__))()(WRITE(PRINT_WRITELN_STREAM_, __VA_ARGS__);) \
 	fputc('\n', PRINT_WRITELN_STREAM_); \
 } while (0)
 #define PRINT(...)   WRITE  (stdout, __VA_ARGS__)
@@ -30,12 +27,16 @@
 		(PRINT_WRITE_STREAM_, PRINT_HEAD_ Args); \
 ) ( \
 	PRINT_WRITE1_(PRINT_WRITE_STREAM_, X) \
-) __VA_OPT__(PRINT_DEFER_(PRINT_WRITE2_)()(PRINT_IF_(PRINT_IS_PACK_(X)) ( \
-	(PRINT_TAIL_ Args) \
-) ( \
-	Args \
-), Peek, __VA_ARGS__))
-#define PRINT_WRITE1_(S, X) _Generic((X), \
+) PRINT_IF_(PRINT_NO_ARGS_(__VA_ARGS__))() ( \
+	PRINT_DEFER_ PRINT_EMPTY_()(PRINT_WRITE2_)() ( \
+		PRINT_IF_(PRINT_IS_PACK_(X)) ( \
+			(PRINT_TAIL_ Args) \
+		) ( \
+			Args \
+		), Peek, __VA_ARGS__))
+#if defined(__STDC__) && defined(__STDC_VERSION__)
+#if 201112L <= __STDC_VERSION__
+	#define PRINT_WRITE1_(S, X) _Generic((X), \
 	int8_t      : fprintf(S, "%"  PRIi8,                     _Generic((X), int8_t      : X, default : (int8_t)      0)), \
 	uint8_t     : fprintf(S, "%"  PRIu8,                     _Generic((X), uint8_t     : X, default : (uint8_t)     0)), \
 	int16_t     : fprintf(S, "%"  PRIi16,                    _Generic((X), int16_t     : X, default : (int16_t)     0)), \
@@ -51,12 +52,17 @@
 	char        : fputc  (                                   _Generic((X), char        : X, default : (char)        0), S), \
 	char       *: fputs  (                                   _Generic((X), char       *: X, default : (char       *)0), S), \
 	char const *: fputs  (                                   _Generic((X), char const *: X, default : (char const *)0), S), \
-	default     : fprintf(S, "%p",                                                                (void *)(uintptr_t) (X)));
+	default     : fprintf(S, "%p",                                                      (void const *)(uintptr_t) (X)));
+#endif
+#endif
+#ifndef PRINT_WRITE1_
+		#define PRINT_WRITE1_(S, X) fputs(X, S);
+#endif
 #define PRINT_WRITE2_() PRINT_WRITE0_
 #define PRINT_WRITE3_(X, ...) \
 	PRINT_WRITE1_(PRINT_WRITE_STREAM_, X) \
-__VA_OPT__ ( \
-	PRINT_DEFER_(PRINT_WRITE4_)()(__VA_ARGS__) \
+PRINT_IF_(PRINT_NO_ARGS_(__VA_ARGS__))() ( \
+	PRINT_DEFER_ PRINT_EMPTY_()(PRINT_WRITE4_)()(__VA_ARGS__) \
 )
 #define PRINT_WRITE4_() PRINT_WRITE3_
 
@@ -72,7 +78,12 @@ __VA_OPT__ ( \
 #define PRINT_EVAL2_(...)      PRINT_EVAL3_(PRINT_EVAL3_(PRINT_EVAL3_(__VA_ARGS__)))
 #define PRINT_EVAL3_(...)      PRINT_EVAL4_(PRINT_EVAL4_(PRINT_EVAL4_(__VA_ARGS__)))
 #define PRINT_EVAL4_(...)      PRINT_EVAL5_(PRINT_EVAL5_(PRINT_EVAL5_(__VA_ARGS__)))
-#define PRINT_EVAL5_(...)      __VA_ARGS__
+#ifdef _MSC_VER_
+	#define PRINT_EVAL5_(...)  PRINT_EVAL6_(PRINT_EVAL6_(__VA_ARGS__))
+	#define PRINT_EVAL6_(...)  __VA_ARGS__
+#else
+	#define PRINT_EVAL5_(...)  __VA_ARGS__
+#endif
 #define PRINT_HEAD_(X, ...)    X
 #define PRINT_IF_(...)         PRINT_CONCAT_ ( \
 	PRINT_CONCAT_(PRINT_IF, __VA_ARGS__), _)
@@ -82,20 +93,19 @@ __VA_OPT__ ( \
 #define PRINT_IS_PACK0_(...)   PRINT_SND_(__VA_ARGS__, 0,)
 #define PRINT_IS_PACK1_(...)   ~, 1
 #define PRINT_LAST_(...)       PRINT_EVAL_(PRINT_LAST0_(__VA_ARGS__))
-#define PRINT_LAST0_(X, ...)   PRINT_VA_OPT_(__VA_OPT__(1), \
+#define PRINT_LAST0_(X, ...)   PRINT_IF_(PRINT_NO_ARGS_(__VA_ARGS__)) ( \
 	X \
-) __VA_OPT__ ( \
-	PRINT_DEFER_(PRINT_LAST1_)()(__VA_ARGS__) \
+) ( \
+	PRINT_DEFER_ PRINT_EMPTY_()(PRINT_LAST1_)()(__VA_ARGS__) \
 )
 #define PRINT_LAST1_()         PRINT_LAST0_
+#define PRINT_NO_ARGS_(...) PRINT_IS_PACK_ ( \
+	PRINT_IF_(PRINT_IS_PACK_(PRINT_HEAD_(__VA_ARGS__)))(~)( \
+		PRINT_HEAD_(__VA_ARGS__) \
+	)())
 #define PRINT_SND_(X, Y, ...)  Y
 #define PRINT_STR_(...)        PRINT_STR__(__VA_ARGS__)
 #define PRINT_STR__(...)       #__VA_ARGS__
 #define PRINT_TAIL_(_, ...)    __VA_ARGS__ 
-#define PRINT_VA_OPT_(Cond, ...) PRINT_CONCAT_ ( \
-	PRINT_CONCAT_(PRINT_VA_OPT, Cond), __)(__VA_ARGS__)
-#define PRINT_VA_OPT__(...)    __VA_ARGS__
-#define PRINT_VA_OPT0__(...)   __VA_ARGS__
-#define PRINT_VA_OPT1__(...)
 
 #endif
